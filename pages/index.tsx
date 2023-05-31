8ac7c3080652b62d0929f7ddc33f4e9d9e512af6
Jomiloju Oke <jomilojuforlife2004@gmail.com>
2023-05-31 17:32:01 +0100

    i created the hero session✌ and i created a background circle components✌

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import React from "react";
 
 function Home() {
     return (
-        <div>
+        <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
             {/* Header  */}
             <Header />
 
             {/* hero */}
+            <section id="hero">
+                <Hero />
+            </section>
 
             {/* about */}
 
